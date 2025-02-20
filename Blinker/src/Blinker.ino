f1f969928b5c95e734563b8d16fa456d98d20974
Hobomilo <hobomilo2007@gmail.com>
2025-02-20 12:52:55 -0700

    Update Blinker.ino

--- Blinker/src/Blinker.ino
+++ Blinker/src/Blinker.ino
@@ -1,37 +1,41 @@
 #include <Arduino.h>
 
-void setup() {
-pinMode(LED_BUILTIN, OUTPUT);
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-//R
-dot();
-dash();
-dot();
-delay(500);
-//E
-dot();
-delay(500);
-//I
-dot();
-dot();
-delay(500);
-//D
-dash();
-dot();
-dot();
-delay(500);
+void loop()
+{
+    // R
+    dot();
+    dash();
+    dot();
+    delay(500);
+    // E
+    dot();
+    delay(500);
+    // I
+    dot();
+    dot();
+    delay(500);
+    // D
+    dash();
+    dot();
+    dot();
+    delay(500);
 }
-void dash() {
-digitalWrite(LED_BUILTIN, HIGH);
-delay(300);
-digitalWrite(LED_BUILTIN, LOW);
-delay(200);
+void dash()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(300);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
 }
-void dot() {
-digitalWrite(LED_BUILTIN, HIGH);
-delay(100);
-digitalWrite(LED_BUILTIN, LOW);
-delay(200);
+void dot()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
 }
